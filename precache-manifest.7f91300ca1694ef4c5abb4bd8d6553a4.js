b47775cfea9a00e77f030cd50c76ac4ec15189af
spry_ps <shafieearash2000@yahoo.com>
2021-07-30 10:27:25 -0400

    Updates

---
+++ precache-manifest.7f91300ca1694ef4c5abb4bd8d6553a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdde03fd6ea0fbfd9682874329e8f1c",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "aad6bf7890250a0d2b1f",
+    "url": "/random-quote/static/css/main.ce1091b4.chunk.css"
+  },
+  {
+    "revision": "34e2f0737faff0d735cd",
+    "url": "/random-quote/static/js/2.be13eb05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote/static/js/2.be13eb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad6bf7890250a0d2b1f",
+    "url": "/random-quote/static/js/main.d58a6f55.chunk.js"
+  },
+  {
+    "revision": "281c3fdc18e22c6a427d",
+    "url": "/random-quote/static/js/runtime-main.ce5e683c.js"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/random-quote/static/media/twitter.6620c8ae.svg"
+  }
+]);
